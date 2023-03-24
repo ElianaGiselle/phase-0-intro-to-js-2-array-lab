542ac7dbbf151b5a49d91a59ca2cd407806f7d35
Eliana Echeverria <eliana.giselle82@yahoo.com>
2023-03-24 16:44:44 -0700

    initial commit

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,5 +1,6 @@
 require ( './helpers.js' );
 
+
 describe('index.js', function () {
   describe('cats', function () {
     it('is assigned an initial value of ["Milo", "Otis", "Garfield"]', function () {
@@ -79,3 +80,57 @@ describe('index.js', function () {
     });
   });
 });
+
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+
+
+function destructivelyAppendCat(name){
+  cats.push(name);
+}
+
+
+
+function destructivelyPrependCat(name){
+  cats.unshift(name);
+}
+
+
+
+
+function destructivelyRemoveLastCat(){
+   cats.pop();
+}
+
+
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+
+function appendCat(name){
+   const Cats = cats.slice();
+   Cats.push(name);
+   return Cats;
+}
+
+function prependCat(name){
+   const Cats = cats.slice();
+   Cats.unshift(name);
+   return Cats;
+}
+
+function removeLastCat(name){
+   const Cats = cats.slice();
+   Cats.pop(name);
+   return Cats;
+}
+
+function removeFirstCat(name){
+   const Cats = cats.slice();
+   Cats.shift(name);
+   return Cats;
+}
